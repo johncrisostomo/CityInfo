93d2e4770f935cb80a055db4bf4f2fbed5323aef
John Crisostomo <johncrisostomo@protonmail.ch>
2018-04-17 22:19:28 +0800

    Stubs CitiesController

---
+++ CityInfo.API/Controllers/CitiesController.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace CityInfo.API.Controllers
+{
+    public class CitiesController : Controller
+    {
+
+    }
+}
