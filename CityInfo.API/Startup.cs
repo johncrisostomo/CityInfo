85ca254fe32663756d202d60b7e867e7221b7570
John Crisostomo <johncrisostomo@protonmail.ch>
2018-04-17 22:30:54 +0800

    Remove sample exception

--- CityInfo.API/Startup.cs
+++ CityInfo.API/Startup.cs
@@ -32,11 +32,6 @@ namespace CityInfo.API
 
             app.UseMvc();
 
-            app.Run((context) =>
-            {
-                throw new Exception("Example exception");
-            });
-
             //app.Run(async (context) =>
             //{
             //    await context.Response.WriteAsync("Hello World!");
